fc921de536643cfb7b0dbd371c7ce0e134b101d1
unknown <ralphdoncaster@gmail.com>
2015-12-16 21:55:37 -0400

    rewrite in asm, using lw asm call api

--- libs/eelog.h
+++ libs/eelog.h
@@ -1,26 +1,19 @@
 /* avr eeprom debug logging
  * http://nerdralph.blogspot.ca
  * @author: Ralph Doncaster 2015
- * define ISR_SAFE before including eelog.h if interrupts are enabled
  */
 
 #include <avr/io.h>
-#include <avr/interrupt.h>
 #include <avr/eeprom.h>
 
-void eelog(char logdata)
+inline void eelog(char logdata)
 {
-    eeprom_busy_wait();
-    EEARL = (EEARL + 1);
-    EEDR = logdata;
-#ifdef ISR_SAFE
-    cli();
-#endif
-    EECR |= (1<<EEMPE);
-    EECR |= (1<<EEPE);
-#ifdef ISR_SAFE
-    sei();
-#endif
+    // no output, x register input, no clobbers
+    asm volatile ( 
+    "rcall eelog_\n"
+    :
+    : "x" (logdata)
+    : );
 }
 
 __attribute__ ((naked))\
